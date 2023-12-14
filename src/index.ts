c294ee2e42251edecccda2384eb58c93da7b8827
rafamagalhas <magalhas.rafa@gmail.com>
2023-12-14 15:16:50 -0300

    fix: use adapters as providers to core funcs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import UserLogin from "./user/service/UserLogin.ts";
-import UserRegister from "./user/service/UserRegister.ts";
+// import CriptoInverter from "./adapters/CriptoInverter.ts";
+import CriptoReal from "./adapters/CriptoReal.ts";
+import UserCollectionMemory from "./adapters/UserCollectionMemory.ts";
+import UserLogin from "./core/user/service/UserLogin.ts";
+import UserRegister from "./core/user/service/UserRegister.ts";
 
-const register = new UserRegister();
+const criptoProvider = new CriptoReal();
+const userCollection = new UserCollectionMemory();
+const register = new UserRegister(userCollection, criptoProvider);
 
-register.execute({
+await register.execute({
     name: 'John Doe',
     email: 'john@john.land',
     pass: '123456'
 });
 
-const login = new UserLogin();
+const login = new UserLogin(userCollection, criptoProvider);
 const user = await login.execute({
     email: 'john@john.land',
     pass: '123456'
